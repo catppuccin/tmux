9f25bbbf659937ff1cbcf37afb484b6ac7468a93
Zachary Taylor <zacharytaylor@tutamail.com>
2023-11-08 13:29:56 -0600

    allow modification of current window bg color

--- window/window_current_format.sh
+++ window/window_current_format.sh
@@ -1,6 +1,6 @@
 show_window_current_format() {
   local number="#I"
-  local color="$thm_orange"
+  local color=$(get_tmux_option "@catppuccin_window_current_color" "$thm_orange")
   local background="$thm_bg"
   local text="$(get_tmux_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
   local fill="$(get_tmux_option "@catppuccin_window_current_fill" "number")" # number, all, none
