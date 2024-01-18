fc7b6c18503da70a0fcc6dbc8291bf6c4043b65a
Zachary Taylor <zacharytaylor@tutamail.com>
2024-01-18 09:53:19 -0600

    feat: adding default window color option

--- window/window_default_format.sh
+++ window/window_default_format.sh
@@ -1,6 +1,6 @@
 show_window_default_format() {
   local number="#I"
-  local color="$thm_blue"
+  local color=$(get_tmux_option "@catppuccin_window_default_color" "$thm_blue")
   local background=$(get_tmux_option "@catppuccin_window_default_background" "$thm_gray")
   local text="$(get_tmux_option "@catppuccin_window_default_text" "#{b:pane_current_path}")" # use #W for application instead of directory
   local fill="$(get_tmux_option "@catppuccin_window_default_fill" "number")" # number, all, none
