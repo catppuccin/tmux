f689db7cd77e592b2ad96b5f1541d82061d88b80
Muhammad Zafar <xafarr@gmail.com>
2024-03-15 17:14:10 +1100

    Update tmux option names according to the clima module

--- status/clima.sh
+++ status/clima.sh
@@ -1,9 +1,9 @@
 # Requires https://github.com/vascomfnunes/tmux-clima
 show_clima() {
   local index=$1
-  local icon="$(get_tmux_option "@catppuccin_weather_icon" "")"
-  local color="$(get_tmux_option "@catppuccin_weather_color" "$thm_yellow")"
-  local text="$(get_tmux_option "@catppuccin_weather_text" "#{clima}")"
+  local icon="$(get_tmux_option "@catppuccin_clima_icon" "")"
+  local color="$(get_tmux_option "@catppuccin_clima_color" "$thm_yellow")"
+  local text="$(get_tmux_option "@catppuccin_clima_text" "#{clima}")"
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
 
