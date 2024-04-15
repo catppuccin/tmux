eddbcbf9b1bec4f0cc18eb6f68e7d99cc0361a98
Mikel Bravo <me@mklbravo.net>
2024-04-15 16:49:30 +0200

    feat(window-format): send window type param to builder

--- window/window_default_format.sh
+++ window/window_default_format.sh
@@ -6,8 +6,9 @@ show_window_default_format() {
   background=$(get_tmux_option "@catppuccin_window_default_background" "$thm_gray")
   text="$(get_tmux_option "@catppuccin_window_default_text" "#{b:pane_current_path}")" # use #W for application instead of directory
   fill="$(get_tmux_option "@catppuccin_window_default_fill" "number")"                 # number, all, none
+  type="default"
 
-  default_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill")
+  default_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill" "$middle_seprator" "$type")
 
   echo "$default_window_format"
 }
