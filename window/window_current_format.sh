dac1a87fc3f60e562b9765555e54e1fe9e2d09a4
Mikel Bravo <me@mklbravo.net>
2024-04-15 16:49:30 +0200

    feat(window-format): send window type param to builder

--- window/window_current_format.sh
+++ window/window_current_format.sh
@@ -6,8 +6,9 @@ show_window_current_format() {
   background=$(get_tmux_option "@catppuccin_window_current_background" "$thm_bg")
   text="$(get_tmux_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
   fill="$(get_tmux_option "@catppuccin_window_current_fill" "number")"                 # number, all, none
+  type="current"
 
-  current_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill")
+  current_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill" "$type")
 
   echo "$current_window_format"
 }
