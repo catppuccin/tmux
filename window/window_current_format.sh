807e19b310188040985416ae602cc3314fe903c6
Mikel Bravo <me@mklbravo.net>
2024-04-15 16:49:30 +0200

    feat(window-format): send window type param to builder
    fix: apply suggestions from code review

--- window/window_current_format.sh
+++ window/window_current_format.sh
@@ -18,7 +18,7 @@ show_window_current_format() {
   text="$(get_tmux_batch_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
   fill="$(get_tmux_batch_option "@catppuccin_window_current_fill" "number")"                 # number, all, none
 
-  current_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill")
+  current_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill" "current")
 
   echo "$current_window_format"
 }
--- window/window_default_format.sh
+++ window/window_default_format.sh
@@ -18,7 +18,7 @@ show_window_default_format() {
   text="$(get_tmux_batch_option "@catppuccin_window_default_text" "#{b:pane_current_path}")" # use #W for application instead of directory
   fill="$(get_tmux_batch_option "@catppuccin_window_default_fill" "number")"                 # number, all, none
 
-  default_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill")
+  default_window_format=$(build_window_format "$number" "$color" "$background" "$text" "$fill" "default")
 
   echo "$default_window_format"
 }
