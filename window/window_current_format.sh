5f494c0d9385dea328e921d8ee530dd509dd78d1
Ninzalo <k-artemiy@mail.ru>
2024-07-02 01:42:56 +0300

    feat: update gruvbox theme bg color

--- window/window_current_format.sh
+++ window/window_current_format.sh
@@ -3,7 +3,7 @@ show_window_current_format() {
 
   number="#I"
   color=$(get_tmux_option "@catppuccin_window_current_color" "$thm_orange")
-  background=$(get_tmux_option "@catppuccin_window_current_background" "$thm_bg")
+  background=$(get_tmux_option "@catppuccin_window_current_background" "#504945")
   text="$(get_tmux_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
   fill="$(get_tmux_option "@catppuccin_window_current_fill" "number")"                 # number, all, none
 
