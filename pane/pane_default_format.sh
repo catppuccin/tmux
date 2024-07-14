5b335912fcd59efa529f8a3733b6b1760aaebb4e
vdbewout <vdbewout@gmail.com>
2024-07-14 15:33:45 +0000

    feat: batch tmux show options

--- pane/pane_default_format.sh
+++ pane/pane_default_format.sh
@@ -2,10 +2,10 @@ show_pane_default_format() {
   local number color background text fill
 
   number="#{pane_index}"
-  color="$(get_tmux_option "@catppuccin_pane_color" "$thm_green")"
-  background="$(get_tmux_option "@catppuccin_pane_background_color" "$thm_gray")"
-  text="$(get_tmux_option "@catppuccin_pane_default_text" "#{b:pane_current_path}")"
-  fill="$(get_tmux_option "@catppuccin_pane_default_fill" "number")" # number, all, none
+  color="$(get_tmux_batch_option "@catppuccin_pane_color" "$thm_green")"
+  background="$(get_tmux_batch_option "@catppuccin_pane_background_color" "$thm_gray")"
+  text="$(get_tmux_batch_option "@catppuccin_pane_default_text" "#{b:pane_current_path}")"
+  fill="$(get_tmux_batch_option "@catppuccin_pane_default_fill" "number")" # number, all, none
 
   default_pane_format=$(build_pane_format "$number" "$color" "$background" "$text" "$fill")
 
