ebca4871555e7051430bd21eac4d79c6f552b037
Iván Arjona Alonso <ivanarjonaalonso@gmail.com>
2024-07-15 14:23:34 +0200

    Use color variables

--- status/kube.sh
+++ status/kube.sh
@@ -6,8 +6,8 @@ show_kube() {
   index=$1
   icon=$(get_tmux_option "@catppuccin_kube_icon" "󱃾")
   color=$(get_tmux_option "@catppuccin_kube_color" "$thm_blue")
-  context_color=$(get_tmux_option "@catppuccin_kube_context_color" "red")
-  namespace_color=$(get_tmux_option "@catppuccin_kube_namespace_color" "cyan")
+  context_color=$(get_tmux_option "@catppuccin_kube_context_color" "\\$thm_red")
+  namespace_color=$(get_tmux_option "@catppuccin_kube_namespace_color" "\\$thm_cyan")
   text=$(get_tmux_option "@catppuccin_kube_text" "#(KUBE_TMUX_SYMBOL_ENABLE=false /bin/bash ${TMUX_PLUGIN_MANAGER_PATH}kube-tmux/kube.tmux 250 $context_color $namespace_color)")
 
   module=$( build_status_module "$index" "$icon" "$color" "$text" )
