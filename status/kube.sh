39d897022d27dcea845e695d6c77942d91352cbd
Iván Arjona Alonso <ivanarjonaalonso@gmail.com>
2024-07-15 14:39:26 +0200

    Not override $KUBE_TMUX_SYMBOL_ENABLE if already set

--- status/kube.sh
+++ status/kube.sh
@@ -1,14 +1,15 @@
 # Requires https://github.com/jonmosco/kube-tmux
 
 show_kube() {
-  local index icon color text  context_color namespace_color module
+  local index icon color text context_color namespace_color symbol_enabled module
 
   index=$1
   icon=$(get_tmux_option "@catppuccin_kube_icon" "󱃾")
   color=$(get_tmux_option "@catppuccin_kube_color" "$thm_blue")
   context_color=$(get_tmux_option "@catppuccin_kube_context_color" "\\$thm_red")
   namespace_color=$(get_tmux_option "@catppuccin_kube_namespace_color" "\\$thm_cyan")
-  text=$(get_tmux_option "@catppuccin_kube_text" "#(KUBE_TMUX_SYMBOL_ENABLE=false ${TMUX_PLUGIN_MANAGER_PATH}kube-tmux/kube.tmux 250 $context_color $namespace_color)")
+  symbol_enabled=${KUBE_TMUX_SYMBOL_ENABLE:-false}
+  text=$(get_tmux_option "@catppuccin_kube_text" "#(KUBE_TMUX_SYMBOL_ENABLE=$symbol_enabled ${TMUX_PLUGIN_MANAGER_PATH}kube-tmux/kube.tmux 250 $context_color $namespace_color)")
 
   module=$( build_status_module "$index" "$icon" "$color" "$text" )
 
