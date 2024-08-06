3c714c7ed0618056f927dca7924543c63dbbffc5
vdbe <44153531+vdbe@users.noreply.github.com>
2024-08-06 13:22:56 +0000

    Revert "perf: batch tmux show-options (#240)"
    This reverts commit 3c6f6f282b3bb17554dc2b4b80760b6507acfd65.

--- status/application.sh
+++ status/application.sh
@@ -1,13 +1,10 @@
 show_application() {
   local index icon color text module
 
-  tmux_batch_setup_status_module "application"
-  run_tmux_batch_commands
-
   index=$1
-  icon=$(get_tmux_batch_option "@catppuccin_application_icon" "")
-  color=$(get_tmux_batch_option "@catppuccin_application_color" "$thm_pink")
-  text=$(get_tmux_batch_option "@catppuccin_application_text" "#{pane_current_command}")
+  icon=$(get_tmux_option "@catppuccin_application_icon" "")
+  color=$(get_tmux_option "@catppuccin_application_color" "$thm_pink")
+  text=$(get_tmux_option "@catppuccin_application_text" "#{pane_current_command}")
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
--- status/uptime.sh
+++ status/uptime.sh
@@ -1,13 +1,10 @@
 show_uptime() {
   local index icon color text module
 
-  tmux_batch_setup_status_module "uptime"
-  run_tmux_batch_commands
-
   index=$1
-  icon="$(get_tmux_batch_option "@catppuccin_uptime_icon" "󰔟")"
-  color="$(get_tmux_batch_option "@catppuccin_uptime_color" "$thm_green")"
-  text="$(get_tmux_batch_option "@catppuccin_uptime_text" "#(uptime | sed 's/^[^,]*up *//; s/, *[[:digit:]]* user.*//; s/ day.*, */d /; s/:/h /; s/ min//; s/$/m/')")"
+  icon="$(get_tmux_option "@catppuccin_uptime_icon" "󰔟")"
+  color="$(get_tmux_option "@catppuccin_uptime_color" "$thm_green")"
+  text="$(get_tmux_option "@catppuccin_uptime_text" "#(uptime | sed 's/^[^,]*up *//; s/, *[[:digit:]]* user.*//; s/ day.*, */d /; s/:/h /; s/ min//; s/$/m/')")"
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
