5a73c551ee7b9c6494e6d54a4f9506f325344b7e
Kaley Main <kaleymain@google.com>
2024-09-08 16:29:31 +1000

    Add a test helper script

--- tests/default_options.sh
+++ tests/default_options.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+source ${script_dir}/helpers.sh
 
 # Tests that the default options are set correctly
 tmux source "${script_dir}/../catppuccin_options_tmux.conf"
 tmux source "${script_dir}/../catppuccin_tmux.conf"
 
-tmux show -g @catppuccin_flavor
-
-echo -n "\n@catppuccin_menu_selected_style "
-tmux display-message -p '#{E:@catppuccin_menu_selected_style}'
+print_option @catppuccin_flavor
+print_option @catppuccin_menu_selected_style
+print_option @catppuccin_pane_active_border_style
 
