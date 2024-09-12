b6804c38e854d027d4856d5edb27f2a3b6e76ca1
Kaley Main <kaleymain@google.com>
2024-09-12 19:38:43 +1000

    Fix test errors

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ set -Eeuo pipefail
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-${script_dir}/tests/harness.sh --test ${script_dir}/tests/default_options.sh --expected ${script_dir}/tests/default_options_expected.txt "$@"
+"${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/default_options.sh --expected "${script_dir}"/tests/default_options_expected.txt "$@"
