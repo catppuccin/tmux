d7a706d713e8a03106463d7097f5acc8bdc8c263
Bryan SebaRaj <bryanwsebaraj@gmail.com>
2024-09-13 00:58:05 -0400

    added middle separator for status

--- builder/module_builder.sh
+++ builder/module_builder.sh
@@ -9,6 +9,7 @@ build_status_module() {
   if [ "$status_fill" = "icon" ]; then
     local bg
     local show_icon="#[fg=$thm_bg,bg=$color,nobold,nounderscore,noitalics]$icon "
+    local show_middle_separator="#[fg=$color,bg=$background,nobold,nounderscore,noitalics]$status_middle_separator"
     local show_text="#[fg=$thm_fg,bg=$thm_gray] $text"
 
     if [ "$status_connect_separator" = "yes" ]; then
@@ -44,5 +45,5 @@ build_status_module() {
     show_icon=""
   fi
 
-  echo "$show_left_separator$show_icon$show_text$show_right_separator"
+  echo "$show_left_separator$show_icon$show_middle_separator$show_text$show_right_separator"
 }
