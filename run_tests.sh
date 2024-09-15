97e2bbce7851cae211d6e36b195beba00f1ce1a6
vdbewout <vdbewout@gmail.com>
2024-09-15 20:48:17 +0000

    feat(status): more modules

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,7 @@ set -Eeuo pipefail
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 "${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/default_options.sh --expected "${script_dir}"/tests/default_options_expected.txt "$@"
+
 "${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/application_module.sh --expected "${script_dir}"/tests/application_module_expected.txt "$@"
+"${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/battery_module.sh --expected /dev/null "$@"
+"${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/cpu_module.sh --expected /dev/null "$@"
