06fff2f40ec4a5cd2c9e064bc7fde22130ecd4bb
David Matheson <david.f.matheson@gmail.com>
2024-10-08 23:06:22 -0400

    fix(status): use a literal string for battery_icon so it can be replaced by the battery plugin (#365)
    Fixes #342

--- run_tests.sh
+++ run_tests.sh
@@ -7,6 +7,6 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 "${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/default_options.sh --expected "${script_dir}"/tests/default_options_expected.txt "$@"
 
 "${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/application_module.sh --expected "${script_dir}"/tests/application_module_expected.txt "$@"
-"${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/battery_module.sh --expected /dev/null "$@"
+"${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/battery_module.sh --expected "${script_dir}"/tests/battery_module_expected.txt "$@"
 "${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/cpu_module.sh --expected "${script_dir}"/tests/cpu_module_expected.txt "$@"
 "${script_dir}"/tests/harness.sh --test "${script_dir}"/tests/pane_styling.sh --expected "${script_dir}"/tests/pane_styling_expected.txt "$@"
--- tests/battery_module.sh
+++ tests/battery_module.sh
@@ -8,5 +8,4 @@ source "${script_dir}/helpers.sh"
 tmux source "${script_dir}/../catppuccin_options_tmux.conf"
 tmux source "${script_dir}/../catppuccin_tmux.conf"
 
-print_option @catppuccin_battery_icon | grep -q "#{battery_icon}" ||
-  echo "@catppuccin_status_battery expanded #{battery_icon} in @catppuccin_battery_icon"
+print_option @catppuccin_status_battery
