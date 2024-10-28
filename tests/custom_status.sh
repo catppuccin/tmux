656906ac1ea8d53d74a9b9b31dddccfab537306e
K H Soh <hju37823@outlook.com>
2024-10-28 13:52:46 +0800

    Moved custom subdirectory to avoid .gitignore filter

--- tests/custom_status.sh
+++ tests/custom_status.sh
@@ -4,7 +4,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 # shellcheck disable=SC1091
 source "${script_dir}/helpers.sh"
 
-tmux set -g @catppuccin_user_status_directory "${script_dir}/custom"
+tmux set -g @catppuccin_user_status_directory "${script_dir}/userdefined"
 # Tests that the default options are set correctly
 tmux source "${script_dir}/../catppuccin_options_tmux.conf"
 tmux source "${script_dir}/../catppuccin_tmux.conf"
