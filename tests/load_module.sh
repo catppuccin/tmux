eef95ae83646590532d10372b849d5b3c1e52f79
Lorenzo Ruffini <lorenzo.ruffini94@gmail.com>
2025-03-22 09:07:02 +0100

    fix: load status panel according to tmux suggestions

---
+++ tests/load_module.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+# shellcheck disable=SC1091
+source "${script_dir}/helpers.sh"
+
+# Tests that the default options are set correctly
+tmux source "${script_dir}/../catppuccin_options_tmux.conf"
+tmux source "${script_dir}/../catppuccin_tmux.conf"
+
+print_option E:@catppuccin_status_load
